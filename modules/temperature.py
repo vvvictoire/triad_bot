b56b30f691f22fa406255089d841a3a12f60ff24
Victoire <vvictoire@pm.me>
2021-10-31 22:21:41 +0100

    Description for C and F commands

--- modules/temperature.py
+++ modules/temperature.py
@@ -10,12 +10,14 @@ class Temperature(commands.Cog):
 
     @commands.command()
     async def c(self, context, temperature):
+        """Converts degrees Farenheit to degrees Celsius"""
         temperature = float(temperature)
         celsius = ltb.far_to_cel(temperature)
         await context.send(f'{temperature}°F = {celsius:.2f}°C')
 
     @commands.command()
     async def f(self, context, temperature):
+        """Converts degrees Celsius to degrees Farenheit"""
         temperature = float(temperature)
         farenheit = ltb.cel_to_far(temperature)
         await context.send(f'{temperature}°C = {farenheit:.2f}°F')
