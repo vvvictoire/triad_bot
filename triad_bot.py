2b9fb1a80fd97d0eb91dd058ff04d9ea692dd234
Victoire <vvictoire@pm.me>
2021-11-21 15:02:44 +0100

    Run in main function

--- triad_bot.py
+++ triad_bot.py
@@ -11,23 +11,27 @@ from modules import config
 
 # Load the config
 CONFIG_FILENAME = 'config.json'
-configuration = config.Config(CONFIG_FILENAME)
-
-# Instanciate the bot
-bot = commands.Bot(command_prefix=configuration.config['command_prefix'],
-                   description=configuration.config['description'])
 
 
-@bot.event
-async def on_ready():
-    """Fires after being connected(?)"""
-    print(f'Logged in as {bot.user}')
+def main():
+    configuration = config.Config(CONFIG_FILENAME)
+    # Instanciate the bot
+    bot = commands.Bot(command_prefix=configuration.config['command_prefix'],
+                       description=configuration.config['description'])
+
+    @bot.event
+    async def on_ready():
+        """Fires after being connected(?)"""
+        print(f'Logged in as {bot.user}')
+
+    bot.add_cog(weather.Weather(bot, configuration))
+    bot.add_cog(money.Money(bot))
+    bot.add_cog(temperature.Temperature(bot))
+    bot.add_cog(time.Time(bot))
+    bot.add_cog(copypasta.Copypasta(bot, configuration))
+    bot.add_cog(admin.Admin(bot, configuration))
+    bot.run(configuration.config['_keys']['token'])
 
 
-bot.add_cog(weather.Weather(bot, configuration))
-bot.add_cog(money.Money(bot))
-bot.add_cog(temperature.Temperature(bot))
-bot.add_cog(time.Time(bot))
-bot.add_cog(copypasta.Copypasta(bot, configuration))
-bot.add_cog(admin.Admin(bot, configuration))
-bot.run(configuration.config['_keys']['token'])
+if __name__ == '__main__':
+    main()
